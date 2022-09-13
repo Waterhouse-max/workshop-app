d9317c96d037824241dc0683536be0c71a449ac3
Steven Waterhouse <steven.waterhouse@siegeltech.io>
2022-09-13 13:01:44 -0400

    step 2

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes, RouterModule} from '@angular/router'
 import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { StatFiltersComponent } from './stat-filters/stat-filters.component';
 
 
+const dashboardRoutes: Routes= [
+  { path: '', component: VideoDashboardComponent},
+]
 
 @NgModule({
   declarations: [
@@ -15,7 +19,7 @@ import { StatFiltersComponent } from './stat-filters/stat-filters.component';
     StatFiltersComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(dashboardRoutes)
   ]
 })
 export class DashboardModule { }
